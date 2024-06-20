9354780185caf9e6f8b94de086479d2a981e7812
alincoln81 <151587661+alincoln81@users.noreply.github.com>
2024-06-20 16:58:38 -0700

    change from href to assign

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -19,7 +19,7 @@ function resetExperience() {
     countDown(false);
 
     //Redirect
-    window.location.href = "https://www.fans.capture.vixi.thefamousgroup.com/submissions?eventId=6cdaf206-e677-49d9-8c2b-f460f1faccdf&isLockedOrientation=true";
+    window.location.assign("https://www.fans.capture.vixi.thefamousgroup.com/submissions?eventId=6cdaf206-e677-49d9-8c2b-f460f1faccdf&isLockedOrientation=true");
 }
 
 function countDown(bool) {
